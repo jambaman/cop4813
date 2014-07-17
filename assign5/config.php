487ed128d012f7a347240e605b1d88a3ddcfa3f6
Steve Jamba <stevejamba@gmail.com>
2014-07-16 22:06:19 -0400

    config

--- assign5/config.php
+++ assign5/config.php
@@ -1,36 +1,49 @@
-<?php
-        $username="n00869815";
-        $password="copn00869815";
-        $hostname="localhost";
-        $database="n00869815";
-        $conn= new mysqli($hostname,$username,$password);
-        $conn->select_db($database) or die ("Unable to select database");
-        $query = "select * from Employee order by LastName";
-        echo $query;
-        $result = mysqli_query($conn, $query);
-        ?>
+ <?php
+include 'functions.php';
+$username="n00869815";
+$password="copn00869815";
+$hostname="localhost";
+$database="n00869815";
+$conn= new mysqli($hostname,$username,$password);
+$conn->select_db($database) or die ("Unable to select database");
+
+    // create and execute query to get batch of records
+    $query = "select * from Employee order by lastName";
+    $result = mysqli_query($conn, $query);
+?>
         <table>
-        <tr><th>Last Name</th><th>First Name</th><th>Department</th><th>Phone</th><th>Email</th><th>Gender</tr><th>Manager</th></tr>
+        <tr><th>Last Name</th><th>First Name</th><th>Department</th><th>Phone Number</th><th>Email</th><th>Gender</th><th>Manager</th></tr>
         <?php
+        //iterate over record set and print
         while($row = mysqli_fetch_row($result))
         {
-                $lName=$row[1];
-                $fName=$row[0];
-                $department=$row[2]
-                $phone=$row[3];
-                $email=$row[4];
-                $gender=$row[5];
-                $manager=$row[6];
-                ?><tr><td><?php
-                echo $lName; ?></td><td><?php
-                echo $fName; ?></td><td><?php
-                echo $department; ?></td><td><?php
-                echo $phone; ?></td><td><?php
-                echo $email; ?></td><td><?php
-                echo $gender; ?></td><td><?php
-                echo $manager; ?></td><tr><?php
-        }
+                $row[0];
+                $lName=$row[2];
+                $fName=$row[1];
+                $department=$row[3];
+                $phone=$row[4];
+                $email=$row[5];
+                $gender=$row[6];
+                $manager=$row[7];
+                ?>
+                <tr>
+                
+                <td><?php echo $lName;?></td>
+                <td><?php echo $fName;?></td>
+                <td><?php echo $department; ?></td>
+                <td><?php echo format_phone($phone); ?></td>
+                <td><?php echo $email; ?></td>
+                <td><?php echo ucfirst(strtolower($gender));?></td>
+                <td><?php echo $manager; ?></td>
+                <td padding: 5px;><a href="edit.php?id=<?php echo $row[0];?>">Edit</a></td>
+                <td padding: 5px;><a href="delete.php?id=<?php echo $row[0];?>" onclick="return confirm('Are you sure you want to delete this employee?');">Delete</a></td>
+                </tr>
+     
+        <?php 
+        
+        mysqli_close($conn); 
 
-        mysqli_close($conn); ?>
+        }?>
         </table>
+       
 
