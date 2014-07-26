8fc8e26ee844dd1d256be54698cf776b8a65c10a
Steve Jamba <stevejamba@gmail.com>
2014-07-25 22:16:49 -0400

    index php changes

--- Group_Project/PHP/register.php
+++ Group_Project/PHP/register.php
@@ -5,34 +5,25 @@
                     <div class="col-1">
  
                         <label >First Name</label>
-
                         <label >Last Name</label>
-                        
                         <label >Email</label>
- 
-                       
-                        
-                         <label>Choose a Password
+						<label>Choose a Password
                         <span class="small">At least 1 letter and 1 number</span>
                         </label>
- 
-                         <label>Reenter Password
-                        <span class="small">Confirm Password</span>
+                       
                         </label>
-                        
                     </div>
                     <div class="col-2">
  
                         <input type="text" name="firstname" id="fname" required/>
                         <input type="text" name="lastname" id="lname" required/>
- 
                         <input type="text" name="email" id="email"  required />
 						<input type="password" name="password1" id="password1"  required />
 						<input type="password" name="password2" id="password2"  required />
-                        </div>
+					</div>
  
                     <center>
-                      <button type="submit">Add Employee</button>
+                      <button type="submit">Register Account</button>
                     </center>
  
             </form>
